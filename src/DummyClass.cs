63a78532517c96011498b1fe2a13462f6fc91f13
tebjan <tebjan@vvvv.org>
2018-10-01 19:29:46 +0200

    added DummyClass.cs so NetMQ package gets picked up by the build process

---
+++ src/DummyClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetMQ.Sockets;
+
+namespace VL.IO.NetMQ
+{
+    class DummyClass
+    {
+        PushSocket s;
+    }
+}
